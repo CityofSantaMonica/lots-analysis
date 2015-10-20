4c0cfb3bd65f69764afc17c171d1baed587311a3
Kegan Maher <kegan.maher@smgov.net>
2015-10-20 16:07:01 -0700

    some basic data cleaning

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os, os.path
+import pandas as pd
 import urllib
 import time
 
-def refreshData(file_path):
+def refresh(file_path):
     url = "https://data.smgov.net/api/views/ng8m-khuz/rows.csv?accessType=DOWNLOAD"
 
-    def _refreshData():
+    def _refresh():
         print "retrieving data file"
         urllib.urlretrieve(url, file_path)
 
@@ -14,12 +15,33 @@ def refreshData(file_path):
         mtime = os.stat(file_path).st_mtime
         now = time.time()
         if ((now - mtime) / 60) > 60:
-            _refreshData(file_path)
+            _refresh()
         else:
             print "file is fresh"
     else:
-        _refreshData(file_path)
+        _refresh()
+
+def munge(file_path):
+    # read in the latest source data
+    df = pd.read_csv(file_path).head(100000)
+
+    # renaming some funky columns
+    df.rename(
+        columns = { "Date/Time": "DateTime", "Map Point": "MapPoint" },
+        inplace = True
+    )
+
+    # clean up the addresses to match the static data
+    df.Address = df.Address.replace(
+        [r", Santa Monica", r"\.", r"\bSt\b", r"\sat 4th Street"],
+        [               "",    "",  "Street",                 ""],
+        regex = True
+    )
+
+    # convert strings to datetime64[ns] objects
+    df.DateTime = pd.to_datetime(df.DateTime, format = "%m/%d/%Y %H:%M:%S %p")
 
 if __name__ == "__main__":
     data_file = "./data.csv"
-    refreshData(data_file)
+    refresh(data_file)
+    munge(data_file)
